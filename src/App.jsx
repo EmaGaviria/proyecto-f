fb417f0170abb4d67a17c8b451186002acb8e296
EmaGaviria <emmanuelgaviria02@gmail.com>
2025-11-21 23:14:43 -0500

    componente app.jsx finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
-import { useState } from 
-import BarraNavegacion from 
-import FiltroCategorias from 
-import ListaJuegos from 
-import { juegos } from 
+import { useState } from 'react';
+import BarraNavegacion from './components/NavBar';
+import FiltroCategorias from './components/CategoryFilter';
+import ListaJuegos from './components/GameList';
+import { juegos } from "./data/games.js";
 
+export default function App() {
+  const [busqueda, setBusqueda] = useState('');
+  const [categoria, setCategoria] = useState('Todos');
+
+  const juegosFiltrados = juegos.filter(j =>
+    j.titulo.toLowerCase().includes(busqueda.toLowerCase()) &&
+    (categoria === 'Todos' || j.genero === categoria)
+  );
+
+  return (
+    <div className="contenedor-principal">
+      <BarraNavegacion setBusqueda={setBusqueda} />
+
+      <div className="zona-contenido">
+        <FiltroCategorias categoria={categoria} setCategoria={setCategoria} />
+        <ListaJuegos juegos={juegosFiltrados} />
+      </div>
+    </div>
+  );
+}
