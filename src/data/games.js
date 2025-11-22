f95f978facfdffc51ece3a14517f966739f38f2f
EmaGaviria <emmanuelgaviria02@gmail.com>
2025-11-21 23:06:15 -0500

    Datos de los juegos

---
+++ src/data/games.js
@@ -0,0 +1,135 @@
+ export const juegos = [
+  {
+    _id: "1",
+    titulo: "Roblox",
+    genero: "Aventura",
+    plataforma: "Nintendo Switch",
+    añoLanzamiento: 2017,
+    desarrollador: "Nintendo",
+    imagenPortada: "/img/roblox.png", 
+    descripcion: "Un mundo abierto lleno de libertad donde puedes explorar, luchar y descubrir secretos.",
+    
+  },
+  {
+    _id: "2",
+    titulo: "Free Fire",
+    genero: "Acción",
+    plataforma: "PlayStation",
+    añoLanzamiento: 2018,
+    desarrollador: "Santa Monica Studio",
+    imagenPortada: "https://i.imgur.com/2yaf2wb.jpeg",
+    descripcion: "Kratos y su hijo Atreus se embarcan en una aventura épica entre dioses nórdicos.",
+    
+  },
+  {
+    _id: "3",
+    titulo: "Minecraft",
+    genero: "Sandbox",
+    plataforma: "PC",
+    añoLanzamiento: 2011,
+    desarrollador: "Mojang",
+    imagenPortada: "",
+    descripcion: "Construye, explora y sobrevive en un mundo abierto de bloques infinitos.",
+    
+  },
+  {
+    _id: "4",
+    titulo: "The Witcher 3: Wild Hunt",
+    genero: "RPG",
+    plataforma: "PC",
+    añoLanzamiento: 2015,
+    desarrollador: "CD Projekt Red",
+    imagenPortada: "",
+    descripcion: "Geralt busca a Ciri en un mundo oscuro lleno de monstruos, política y decisiones difíciles.",
+   
+  },
+  {
+    _id: "5",
+    titulo: "Fortnite",
+    genero: "Battle Royale",
+    plataforma: "PC",
+    añoLanzamiento: 2017,
+    desarrollador: "Epic Games",
+    imagenPortada: "",
+    descripcion: "Compite contra 100 jugadores en partidas rápidas y llenas de acción.",
+    
+  },
+  {
+    _id: "6",
+    titulo: "Red Dead Redemption 2",
+    genero: "Acción",
+    plataforma: "PlayStation",
+    añoLanzamiento: 2018,
+    desarrollador: "Rockstar Games",
+    imagenPortada: "",
+    descripcion: "Un western impresionante que sigue la historia de Arthur Morgan en un mundo abierto detallado.",
+    
+  },
+  {
+    _id: "7",
+    titulo: "Overwatch",
+    genero: "Shooter",
+    plataforma: "PC",
+    añoLanzamiento: 2016,
+    desarrollador: "Blizzard",
+    imagenPortada: "",
+    descripcion: "Shooter por equipos con héroes únicos y un estilo visual muy llamativo.",
+    
+  },
+  {
+    _id: "8",
+    titulo: "Halo Infinite",
+    genero: "Shooter",
+    plataforma: "Xbox",
+    añoLanzamiento: 2021,
+    desarrollador: "343 Industries",
+    imagenPortada: "",
+    descripcion: "El Jefe Maestro regresa en una aventura épica con un nuevo mundo semiabierto.",
+   
+  },
+  {
+    _id: "9",
+    titulo: "Grand Theft Auto V",
+    genero: "Acción",
+    plataforma: "PC",
+    añoLanzamiento: 2013,
+    desarrollador: "Rockstar Games",
+    imagenPortada: "",
+    descripcion: "Tres protagonistas, un mundo enorme y millones de posibilidades.",
+    
+  },
+  {
+    _id: "10",
+    titulo: "League of Legends",
+    genero: "MOBA",
+    plataforma: "PC",
+    añoLanzamiento: 2009,
+    desarrollador: "Riot Games",
+    imagenPortada: "",
+    descripcion: "Juego competitivo 5v5 donde dos equipos luchan por destruir el nexo enemigo.",
+    
+  },
+  {
+    _id: "11",
+    titulo: "Super Mario Odyssey",
+    genero: "Plataformas",
+    plataforma: "Nintendo Switch",
+    añoLanzamiento: 2017,
+    desarrollador: "Nintendo",
+    imagenPortada: "",
+    descripcion: "Acompaña a Mario alrededor del mundo para rescatar a la princesa Peach.",
+   
+  },
+  {
+    _id: "12",
+    titulo: "Cyberpunk 2077",
+    genero: "RPG",
+    plataforma: "PC",
+    añoLanzamiento: 2020,
+    desarrollador: "CD Projekt Red",
+    imagenPortada: "",
+    descripcion: "Un mundo futurista lleno de acción, tecnología y decisiones que cambian la historia.",
+   
+  }
+
+];
